371a4e6fb32cecd51dd48255aa04debccc08c8c2
Ngoc Thang <huynhngocthanguit@gmail.com>
2017-08-05 18:57:17 +0700

    My project setup completed successfully

---
+++ thpthoabinhbackend/src/main/java/edu/vn/thpthoabinhbackend/App.java
@@ -0,0 +1,13 @@
+package edu.vn.thpthoabinhbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
